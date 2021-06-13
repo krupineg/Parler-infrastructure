90e5856d633bda0e0905691c568ff84266787b34
Yevhen Krupin <krupineg@gmail.com>
2021-06-13 17:14:22 +0200

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(parlr.me www.parlr.me)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="krupineg@gmail.com" 
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
